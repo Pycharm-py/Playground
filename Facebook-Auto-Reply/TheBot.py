5c360c13e797671459480a331abcec291c9b8b71
PyCharm_Py <61084121+Pycharm-py@users.noreply.github.com>
2020-06-15 19:05:40 +0200

    Add files via upload

---
+++ Facebook-Auto-Reply/TheBot.py
@@ -0,0 +1,35 @@
+import pyautogui
+import datetime
+import time
+while True:
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    start = '00:00:00'  # midnight
+    end = '09:00:00'  # 9 A.M.
+    x = pyautogui.locateOnScreen("Xbutton.png", confidence=0.8)
+    message = pyautogui.locateOnScreen("TheMessagePoll.png", confidence=0.8)
+    # If current time is between "start" and "end"
+    if start < current_time < end:
+        time.sleep(0.1)
+        pyautogui.moveTo(x)
+        pyautogui.click(x)
+        time.sleep(0.2)
+        pyautogui.click(x=1684, y=165)
+        time.sleep(0.2)
+        pyautogui.moveTo(message)
+        pyautogui.click(message)
+        pyautogui.write("I'm sleeping... Text to me later", interval=0.2)  # the message
+        pyautogui.press('enter')
+        time.sleep(0.2)
+        pyautogui.click(x=1684, y=165)
+    else:
+        time.sleep(0.2)
+        pyautogui.click(x=1684, y=165)
+        pyautogui.moveTo(x)
+        time.sleep(0.1)
+        pyautogui.click(x)
+        time.sleep(0.2)
+        pyautogui.moveTo(message)
+        pyautogui.click(message)
+        time.sleep(0.2)
+        pyautogui.click(x=1684, y=165)
